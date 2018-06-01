4cb4fcdfe4707000e8ed932cf184397a22ce6b0a
Lorraine Soudade <lsoudade@gmail.com>
2018-06-01 17:27:27 +0200

    removed id / unused comments

--- R_controle_qualite/intersect_2_tumors/somatic_fractions_alleliques.R
+++ R_controle_qualite/intersect_2_tumors/somatic_fractions_alleliques.R
@@ -9,26 +9,6 @@ if (length(args)==0) { stop("Input name missing!\n", call.=FALSE) }
 tumor1_name = args[1]
 tumor2_name = args[2]
 
-######################################################
-# tumor1_name = "M662_DA_12323_T_B00JAKC"
-# tumor2_name = "M662_DA_12323_T_B00JAKD"
-# 
-# tumor1_name = "M662_DA_19392_T_B00JAKP"
-# tumor2_name = "M662_DA_19392_T_B00JAKQ"
-# 
-# tumor1_name = "M662_DA_5009_T_B00JAJB"
-# tumor2_name = "M662_DA_5009_T_B00JAJC"
-# 
-# tumor1_name = "M662_DA_6063_T_B00JAJE"
-# tumor2_name = "M662_DA_6063_T_B00JAJF"
-# 
-# tumor1_name = "M662_DA_8329_T_B00JAKF"
-# tumor2_name = "M662_DA_8329_T_B00JAKH"
-# 
-# tumor1_name = "M662_DA_LB110287_T_B00JAK0"
-# tumor2_name = "M662_DA_LB110287_T_B00JAK2"
-######################################################
-
 patient_id = unlist(strsplit(tumor1_name, "_"))[3]
 sample1_id = unlist(strsplit(tumor1_name, "_"))[5]
 sample2_id = unlist(strsplit(tumor2_name, "_"))[5]
