5fd598ddc1a1638825f65ce25921083065280fd3
Lorraine Soudade <soudadel@students.iarc.fr>
2018-06-05 11:00:12 +0200

    added genericity

--- scripts/normalize_vcf.sh
+++ scripts/normalize_vcf.sh
@@ -1,9 +1,16 @@
-for vcffile in `ls /home/pgm/Workspace/MPM/calling_germline_with_genotype/indexed/*.vcf.gz`
-
-do
-
-vcfiden=`basename $vcffile | cut -d. -f1 `
-echo $vcfiden
-~/Programs/vt/vt decompose -s $vcffile | ~/Programs/vt/vt decompose_blocksub -a - | ~/Programs/vt/vt normalize -r /home/pgm/Workspace/MPM/ref_files/hs38dh_all_chr.fasta -q - | ~/Programs/vt/vt uniq - > /home/pgm/Workspace/MPM/calling_germline_with_genotype/normalized/$vcfiden.normalized.vcf
-
-done
+#!/bin/bash
+if [ "$#" -eq  "0" ]
+then
+  echo "###############################################################"
+  echo "## Normalize VCF                                             ##"
+  echo "##                                                           ##"
+  echo "## Missing argument \$1 : path to reference file              ##"
+  echo "###############################################################"
+else
+  for vcffile in `ls *.vcf.gz`
+  do
+    vcfiden=`basename $vcffile | cut -d. -f1 `
+    echo $vcfiden
+    vt decompose -s $vcffile | vt decompose_blocksub -a - | vt normalize -r $1 -q - | vt uniq - > ./$vcfiden.normalized.vcf
+  done
+fi
