90398ab69cf2399cd268c481526aefba7f2ef44d
Lorraine Soudade <soudadel@students.iarc.fr>
2018-06-08 11:07:36 +0200

    now totally generic

--- R_QC/venn/somatic_overlap_venn.R
+++ R_QC/venn/somatic_overlap_venn.R
@@ -2,25 +2,19 @@
 
 library(VennDiagram)
 source("https://gist.githubusercontent.com/mfoll/a4dfbb92068dc559f130/raw/714dc8c2e97987fd4385dcef2722b3ef986d38d6/get_vcf_data.r")
-setwd("/home/pgm/Workspace/MPM/VCF_finaux/somatic_sandbox")
 
 args = commandArgs(trailingOnly = TRUE)
-if (length(args)==0) { stop("Input name missing!\n", call.=FALSE) }
+if (length(args)==0) { stop("Input missing!\n", call.=FALSE) }
 
-tumor1_name = args[1]
-tumor2_name = args[2]
+path_to_tumor1_VCF       = args[1]
+path_to_tumor2_VCF       = args[2]
+path_to_output_directory = args[3]
+patient_id               = args[4]
+sample1_id               = args[5]
+sample2_id               = args[6]
 
-patient_id = unlist(strsplit(tumor1_name, "_"))[3]
-sample1_id = unlist(strsplit(tumor1_name, "_"))[5]
-sample2_id = unlist(strsplit(tumor2_name, "_"))[5]
-
-tumor1_VCFfile = paste(tumor1_name, '.normalized.vcf_multianno.hg38_multianno.txt', sep='')
-tumor2_VCFfile = paste(tumor2_name, '.normalized.vcf_multianno.hg38_multianno.txt', sep='')
-
-tumor1_VCFcontent = read.table(tumor1_VCFfile, sep="\t", header=FALSE, stringsAsFactors=FALSE)
-tumor2_VCFcontent = read.table(tumor2_VCFfile, sep="\t", header=FALSE, stringsAsFactors=FALSE)
-
-generated_files = '/home/pgm/Workspace/MPM/R_controle_qualite/venn/generated_files_somatic/'
+tumor1_VCFcontent = read.table(path_to_tumor1_VCF, sep="\t", header=FALSE, stringsAsFactors=FALSE)
+tumor2_VCFcontent = read.table(path_to_tumor2_VCF, sep="\t", header=FALSE, stringsAsFactors=FALSE)
 
 
 ############################################
@@ -39,7 +33,7 @@ nb_inter  = length(inter)
 ############################################
 ## Stacked bars with tumor 1 & 2
 ############################################
-svg(filename=paste(generated_files, patient_id, "_", sample2_id, "_on_", sample1_id, ".svg", sep=''))
+svg(filename=paste(path_to_output_directory, "/", patient_id, "_", sample2_id, "_on_", sample1_id, ".svg", sep=''))
 grid.newpage()
 draw.pairwise.venn(nb_tumor1, nb_tumor2, nb_inter, category = c(sample1_id, sample2_id), lty = rep("blank", 2), fill = c(rgb(1,0,0,0.4), rgb(0,0,1,0.3)), cat.pos = c(0, 0), cat.dist = rep(0.025, 2))
 dev.off() # close the device
